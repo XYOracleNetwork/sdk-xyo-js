7e263bcbf381e6cc3cf6ce2c006de81e110c8486
Arie Trouw <arie.trouw@xy.company>
2021-05-05 10:51:19 -0700

    Deploy Update

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,17 +1,17 @@
 import typescript from 'rollup-plugin-typescript2'
 
-import pkg from './package.json'
-
 export default {
   input: './src/index.ts',
   output: [
     {
-      file: pkg.main,
+      exports: 'auto',
+      file: './dist/index.cjs.js',
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: pkg.module,
+      exports: 'auto',
+      file: './dist/index.esm.js',
       format: 'es',
       sourcemap: true,
     },
