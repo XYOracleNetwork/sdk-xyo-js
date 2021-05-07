0e5936182f4e35c8bfd802619effe6cff8baa569
Arie Trouw <arie.trouw@xy.company>
2021-05-07 16:13:48 -0700

    tree shaking

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,20 +1,36 @@
 import typescript from 'rollup-plugin-typescript2'
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      exports: 'auto',
-      file: './dist/index.cjs.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      exports: 'auto',
-      file: './dist/index.esm.js',
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript()],
-}
+export default [
+  {
+    external: ['axios', 'rollbar'],
+    input: './src/index.ts',
+    output: [
+      {
+        exports: 'auto',
+        file: './dist/index.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        exports: 'auto',
+        file: './dist/index.js',
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript()],
+  },
+  {
+    external: ['axios', 'rollbar'],
+    input: './src/assertEx.ts',
+    output: [
+      {
+        exports: 'auto',
+        file: './dist/assertEx.js',
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript()],
+  },
+]
