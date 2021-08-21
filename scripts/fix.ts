4f624d1148319d94abf93a21c47895910268cb67
Arie Trouw <arie.trouw@xy.compay>
2021-08-21 14:44:31 -0700

    Factored out scripts to ts-scripts

--- scripts/fix.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import { execFileSync } from 'child_process'
-console.log(`Fix [${process.cwd()}]`)
-execFileSync('yarn', ['eslint', '--ext', '.js,.jsx,.ts,.tsx,.json', './src', '--fix'], { stdio: 'inherit' })
