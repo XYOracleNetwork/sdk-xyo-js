874cc9cb5b6d4eaf5dcb85854047da9c187cb6f5
Arie Trouw <arie.trouw@xylabs.com>
2021-11-09 08:10:09 -0800

    EthAddress

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import ApiEndpoint from './ApiEndpoint'
 import ApiStage from './ApiStage'
 import assertEx from './assertEx'
 import delay from './delay'
+import EthAddress from './EthAddress'
 import getApiStage from './getApiStage'
 import Log from './Log'
 import Override from './Override'
 
-export { ApiClient, ApiEndpoint, ApiStage, assertEx, delay, getApiStage, Log }
+export { ApiClient, ApiEndpoint, ApiStage, assertEx, delay, EthAddress, getApiStage, Log }
 export type { ApiConfig, Override }
---
+++ src/padHex.ts
@@ -0,0 +1,14 @@
+const padHex = (hex: string, byteCount?: number) => {
+  let result = hex
+  if (hex.length % 2 !== 0) {
+    result = `0${hex}`
+  }
+  if (byteCount) {
+    while (result.length / 2 < byteCount) {
+      result = `00${result}`
+    }
+  }
+  return result
+}
+
+export default padHex
