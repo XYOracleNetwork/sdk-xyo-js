1a2c2e78984655bcd34da698ebf0bc6338321c8f
Arie Trouw <arie.trouw@xylabs.com>
2021-11-10 11:32:13 -0800

    exporting bn.js for easy import upstream

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import ApiConfig from './ApiConfig'
 import ApiEndpoint from './ApiEndpoint'
 import ApiStage from './ApiStage'
 import assertEx from './assertEx'
+import BigNumber from './BigNumber'
 import delay from './delay'
 import EthAddress from './EthAddress'
 import getApiStage from './getApiStage'
 import Log from './Log'
 import Override from './Override'
 
-export { ApiClient, ApiEndpoint, ApiStage, assertEx, delay, EthAddress, getApiStage, Log }
+export { ApiClient, ApiEndpoint, ApiStage, assertEx, BigNumber, delay, EthAddress, getApiStage, Log }
 export type { ApiConfig, Override }
