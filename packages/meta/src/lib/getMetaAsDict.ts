6395eec0f6acae483f8435c11a5573a29ef21310
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-07 16:51:28 -0500

    Make constant from delimiter

--- packages/meta/src/lib/getMetaAsDict.ts
+++ packages/meta/src/lib/getMetaAsDict.ts
@@ -1,17 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type StringIndexable = { [key: string]: any }
 
+export const propertyDelimiter = ':'
+
 export const getMetaAsDict = (obj: StringIndexable, parentKey = ''): StringIndexable => {
   let flatRecord: StringIndexable = {}
   for (const key in obj) {
     if (typeof obj[key] === 'object' && obj[key] !== null) {
       // If the value is another object, we want to iterate through its keys as well.
-      const childRecord = getMetaAsDict(obj[key] as StringIndexable, `${parentKey}${key}:`)
+      const childRecord = getMetaAsDict(obj[key] as StringIndexable, `${parentKey}${key}${propertyDelimiter}`)
       flatRecord = { ...flatRecord, ...childRecord }
     } else {
       // Concatenate the key with its parent key.
       const newKey = parentKey ? `${parentKey}${key}` : key
-      const trimmed = newKey.endsWith(':') ? newKey.slice(0, -1) : newKey
+      const trimmed = newKey.endsWith(propertyDelimiter) ? newKey.slice(0, -1) : newKey
       flatRecord[trimmed] = obj[key]
     }
   }
