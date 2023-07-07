2289c58736cbceba2fba0eb8177b3781c80bcc14
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-07 15:06:05 -0500

    Update Twitter meta definitions

--- packages/meta/src/models.ts
+++ packages/meta/src/models.ts
@@ -7,11 +7,27 @@ export interface OpenGraphStructured {
   width?: number
 }
 
+/**
+ * https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/player-card
+ */
 export interface TwitterPlayer {
-  alt?: string
+  /**
+   * HTTPS URL to iFrame player. This must be a HTTPS URL which does not
+   * generate active mixed content warnings in a web browser. The audio or
+   * video player must not require plugins such as Adobe Flash.
+   */
+  '': string
+  /**
+   * Height of iframe in pixels. Used with player card
+   */
   height?: number
+  /**
+   * URL to raw video or audio stream. Used with player card
+   */
   stream?: string
-  url?: string
+  /**
+   * Width of iframe in pixels. Used with player card
+   */
   width?: number
 }
 
@@ -52,23 +68,69 @@ export interface Meta {
     video?: OpenGraphStructuredProperty
   }
   title?: string
+  /**
+   * https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup
+   */
   twitter?: {
     app?: TwitterApp
+    /**
+     * The card type. Used with all cards. Fallback: og:type.
+     * If an og:type, og:title and og:description exist in the markup but
+     * twitter:card is absent, then a summary card may be rendered.
+     */
     card?: 'summary' | 'summary_large_image' | 'app' | 'player'
+
     creator?: {
+      /**
+       * The @username of content creator. Used with summary_large_image cards
+       */
+      '': string
+      /**
+       * Twitter user ID of content creator. Used with summary,
+       * summary_large_image cards
+       */
       id?: string
-      username?: string
     }
+    /**
+     * Description of content (maximum 200 characters). Used with summary,
+     * summary_large_image, player cards. Fallback: og:description.
+     */
     description?: string
     image?: {
+      /**
+       * URL of image to use in the card. Images must be less than 5MB in size.
+       * JPG, PNG, WEBP and GIF formats are supported. Only the first frame of
+       * an animated GIF will be used. SVG is not supported. Used with summary,
+       * summary_large_image, player cards. Fallback: og:image
+       */
+      '': string
+      /**
+       * A text description of the image conveying the essential nature of
+       * an image to users who are visually impaired. Maximum 420
+       * characters. Used with summary, summary_large_image, player cards
+       */
       alt?: string
-      url?: string
     }
     player?: TwitterPlayer
-    site?: {
-      id?: string
-      username?: string
-    }
+    /**
+     * The @username of website. Either twitter:site or twitter:site:id is
+     * required. Used with summary, summary_large_image, app, player
+     * cards
+     */
+    site?:
+      | string
+      | {
+          /**
+           * Same as twitter:site, but the user’s Twitter ID. Either
+           * twitter:site or twitter:site:id is required. Used with
+           * summary, summary_large_image, player cards
+           */
+          id?: string
+        }
+    /**
+     * Title of content (max 70 characters). Used with summary,
+     * summary_large_image, player cards. Fallback: og:title.
+     */
     title?: string
   }
 }
