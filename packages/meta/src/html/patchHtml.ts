e4dcb89a96b40416f9d5aac78c943ce74f9b5c90
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-10 12:58:58 -0500

    Document head patch

--- packages/meta/src/html/patchHtml.ts
+++ packages/meta/src/html/patchHtml.ts
@@ -1,29 +1,27 @@
 import { load } from 'cheerio'
 
-export const mergeDocumentHeads = (htmlA: string, htmlB: string) => {
-  const $ = load(htmlA)
-  const $2 = load(htmlB)
+export const mergeDocumentHeads = (destination: string, source: string) => {
+  const destinationDocument = load(destination)
+  const sourceDocument = load(source)
 
-  // This assumes that both HTML strings have a body tag.
-  // If that's not the case, you should adjust the code accordingly.
+  const destinationHead = destinationDocument('head')
+  const sourceHead = sourceDocument('head')
 
   // For each child node of the second body
-  $2('head')
-    .children()
-    .each((_, element) => {
-      const el = $(element)
+  sourceHead.children().each((_, element) => {
+    const el = destinationDocument(element)
 
-      // Check if the same element exists in the first HTML string
-      const match = $(el[0].tagName)
+    // Check if the same element exists in the first HTML string
+    const match = destinationDocument(el[0].tagName)
 
-      // If it exists, replace it, otherwise append it
-      if (match.length > 0) {
-        match.replaceWith(el)
-      } else {
-        $('body').append(el)
-      }
-    })
+    // If it exists, replace it, otherwise append it
+    if (match.length > 0) {
+      match.replaceWith(el)
+    } else {
+      destinationDocument('head').append(el)
+    }
+  })
 
   // Return the merged HTML
-  return $.html()
+  return destinationDocument.html()
 }
--- packages/meta/src/html/spec/patchHtml.spec.ts
+++ packages/meta/src/html/spec/patchHtml.spec.ts
@@ -4,8 +4,8 @@ describe('patchHtml', () => {
   const cases: [string, string, string][] = [
     [
       '<html><head></head><body></body></html>',
-      '<html><head><meta name="description" content="test" /></head><body>',
-      '<html><head><meta name="description" content="test" /></head><body></body></html>',
+      '<html><head><meta name="description" content="test"></head><body>',
+      '<html><head><meta name="description" content="test"></head><body></body></html>',
     ],
   ]
   it.each(cases)('Generates head meta', (a: string, b: string, expected: string) => {
