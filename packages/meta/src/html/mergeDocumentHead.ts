562c0c395da9fd9e47db4804dc52c1d8c40b4f68
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-07-10 15:40:45 -0500

    Rename for clarity

--- packages/meta/src/html/mergeDocumentHead.ts
+++ packages/meta/src/html/mergeDocumentHead.ts
@@ -1,42 +1,42 @@
 import { load } from 'cheerio'
 
 export const mergeDocumentHead = (destination: string, source: string) => {
-  const $ = load(destination)
-  const $2 = load(source)
+  const $destination = load(destination)
+  const $source = load(source)
 
-  // For each child node of the second head
-  $2('head')
+  // For each child node of the source head
+  $source('head')
     .children()
     .each((_, element) => {
-      const el = $(element)
+      const el = $destination(element)
 
       // Special case for meta tags: We want to match them by the name attribute
       if (el[0].tagName === 'meta') {
         const name = el.attr('name')
         if (name) {
-          const match = $(`head > meta[name="${name}"]`)
+          const match = $destination(`head > meta[name="${name}"]`)
 
           // If it exists, replace it, otherwise append it
           if (match.length > 0) {
             match.replaceWith(el)
             return
           } else {
-            $('head').append(el)
+            $destination('head').append(el)
           }
-        }
-      } else {
-        // For all other elements, just check if the same element exists in the first HTML string
-        const match = $(el[0].tagName)
-
-        // If it exists, replace it, otherwise append it
-        if (match.length > 0) {
-          match.replaceWith(el)
         } else {
-          $('head').append(el)
+          // For all other elements, just check if the same element exists in the first HTML string
+          const match = $destination(el[0].tagName)
+
+          // If it exists, replace it, otherwise append it
+          if (match.length > 0) {
+            match.replaceWith(el)
+          } else {
+            $destination('head').append(el)
+          }
         }
       }
     })
 
   // Return the merged HTML
-  return $.html()
+  return $destination.html()
 }
